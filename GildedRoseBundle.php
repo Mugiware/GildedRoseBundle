d2834f57734ae7b8fc3bf9f917433e7a7bb39dd0
Francisco José Cruz Romanos <grisendo@gmail.com>
2014-03-02 12:27:12 +0100

    Bundle file

---
+++ GildedRoseBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Mugiware\GildedRoseBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Francisco J. Cruz Romanos <grisendo@gmail.com>
+ */
+class MugiwareGildedRoseBundle extends Bundle
+{
+}
